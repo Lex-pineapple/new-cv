4db61da0564d8af7bc0caae449ad6130c466a5de
Lex-pineapple <pineapplepear55@gmail.com>
2026-06-22 11:32:29 +0300

    fix: update github link

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className={styles.root}>
       <div className={styles.wrapper}>
-        <a href="github" className={styles.link}>
+        <a href="https://github.com/Lex-pineapple" className={styles.link}>
           <img src="/github-icon.svg" className={styles.icon} />
           <p>Lex-pineapple</p>
         </a>
